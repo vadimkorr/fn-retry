9cc7441767a4f4b798f0934595b41a78fc56649a
Vadim <korobeinikovv@mail.ru>
2020-07-23 17:53:08 +0300

    Remove unnecessary tests, set name to returned function

--- src/build-retriable.js
+++ src/build-retriable.js
@@ -2,5 +2,7 @@ import { errorMessages } from '../src/config'
 
 export const buildRetriable = retrier => (fn, options) => {
   if (typeof fn !== 'function') throw new Error(errorMessages.FN_TYPE)
-  return (...args) => retrier(fn.bind(null, ...args), options)
+  return function retriable(...args) {
+    return retrier(fn.bind(null, ...args), options)
+  }
 }
