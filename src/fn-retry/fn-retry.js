1fc9f53d3f6370a5cbfd85d83f470bc047d230a1
Vadim <korobeinikovv@mail.ru>
2020-07-24 18:33:49 +0300

    Code cleanup

--- src/fn-retry/fn-retry.js
+++ src/fn-retry/fn-retry.js
@@ -3,12 +3,6 @@ import { get } from '../../src/utils/get'
 import { getMaxCallsCount } from '../../src/fn-retry/get-max-calls-count'
 import { errorMessages, defaultFn } from '../../src/config'
 
-const getWaiterValue = waiter => {
-  if (!waiter) return null
-  const state = waiter.next()
-  return state.done ? null : state.value
-}
-
 export const fnRetry = async (fn, options) => {
   if (typeof fn !== 'function') throw new Error(errorMessages.FN_TYPE)
 
@@ -17,10 +11,10 @@ export const fnRetry = async (fn, options) => {
   const _delays = get(options, 'delays', [])
   const _waiter = get(options, 'waiter', null)
 
-  let waiterValue = true
+  let iteratorDone = false
   let call = 0
 
-  while (_waiter ? waiterValue !== null : call <= _delays.length) {
+  while (_waiter ? !iteratorDone : call <= _delays.length) {
     try {
       return await fn()
     } catch (error) {
@@ -29,12 +23,15 @@ export const fnRetry = async (fn, options) => {
         call: call + 1, // as call starts from 0, show user friendly value
         ...(_waiter ? {} : { maxCalls: getMaxCallsCount(_delays) }),
       })
-      waiterValue = getWaiterValue(_waiter)
-      const waitMs = _waiter
-        ? waiterValue
-        : call < _delays.length
-        ? _delays[call]
-        : 0
+
+      let waitMs = 0
+      if (_waiter) {
+        const { done, value } = _waiter.next()
+        iteratorDone = done
+        waitMs = value
+      } else {
+        waitMs = call < _delays.length ? _delays[call] : 0
+      }
       waitMs && (await wait(waitMs))
     } finally {
       call++
